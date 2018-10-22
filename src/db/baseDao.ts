6651c27d8902c598698b85548b6a9f503229a9fb
zhoutk <zhoutk@189.cn>
2018-10-22 21:54:48 +0800

    db helper to singleton

--- src/db/baseDao.ts
+++ src/db/baseDao.ts
@@ -6,14 +6,15 @@ type DaoType = typeof Dao
 
 export default class BaseDao{
     table: string
-    dao: DaoType
+    static dao: DaoType
     constructor(table?: string){
         this.table = table || '';
-        this.dao = new Dao()
+        if(!BaseDao.dao)
+            BaseDao.dao = new Dao()
     }
     async retrieve(params = {}, fields =[], session = {userid: ''}){
         try {
-            let rs = await this.dao.select(this.table, params, fields);
+            let rs = await BaseDao.dao.select(this.table, params, fields);
             return rs;
         } catch (err) {
             return Promise.reject(global.jsReponse(204, err.message));
