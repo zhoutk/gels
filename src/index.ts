c93ec7b44f608796a408a2b1ebdc62bf6529ded5
zhoutk <zhoutk@189.cn>
2018-11-13 21:03:06 +0800

    index refactor.

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,16 @@ import appIniter from './app'
 (async () => {
     await globInit.init()
     //初始化koa app
-    let app, port = process.env.PORT || G.CONFIGS.port
+    let port = process.env.PORT || G.CONFIGS.port
     try {
-        app = await appIniter.init()
+        let app = await appIniter.init()
+        app.listen(port, () => {
+            G.logger.info(`current running environment is ${G.NODE_ENV}`)
+            G.logger.info(`✅ 启动地址 http://127.0.0.1:${port}`)
+        })
     } catch (e) {
         G.logger.error(e)
     }
       
-    app.listen(port, () => {
-        G.logger.info(`current running environment is ${G.NODE_ENV}`)
-        G.logger.info(`✅ 启动地址 http://127.0.0.1:${port}`)
-    })
     
 })()
