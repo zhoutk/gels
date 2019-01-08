a15e1c2110bddd53c5133867505c5f0b9c818687
zhoutk <zhoutk@189.cn>
2019-01-08 12:58:15 +0800

    update one & base query ok.

--- src/db/mongoDao.ts
+++ src/db/mongoDao.ts
@@ -5,12 +5,15 @@ const url = `mongodb://${G.CONFIGS.dbconfig.db_user}:${G.CONFIGS.dbconfig.db_pas
 const dbName = G.CONFIGS.dbconfig.db_name
 
 export default class MongoDao implements IDao {
-    select(tablename: string, params: object, fields?: string[]): Promise<any> {
+    select(tablename: string, params: any, fields?: string[]): Promise<any> {
+        let { id, ...ps } = params
+        if (id !== undefined)
+            Object.assign(ps, {_id: new ObjectId(id)}) 
         return new Promise((resolve, reject) => {
             MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => { 
                 const db = client.db(dbName)
                 const collection = db.collection(tablename)
-                collection.find({}).toArray(function (err, docs) {
+                collection.find(ps).toArray(function (err, docs) {
                     resolve(G.jsResponse(G.STCODES.SUCCESS, 'query success.', docs))
                 })
                 client.close()
@@ -18,28 +21,6 @@ export default class MongoDao implements IDao {
         })
     }    
     insert(tablename: string, params: object): Promise<any> {
-        return this.execQuery(tablename, params)
-    }
-    update(tablename: string, params: object, id: string | number): Promise<any> {
-        throw new Error('Method not implemented.')
-    }
-    delete(tablename: string, id: string | number): Promise<any> {
-        throw new Error('Method not implemented.')
-    }
-    querySql(sql: string, values: any[], params: object, fields?: string[]): Promise<any> {
-        throw new Error('Method not implemented.')
-    }
-    execSql(sql: string, values: any[]): Promise<any> {
-        throw new Error('Method not implemented.')
-    }
-    insertBatch(tablename: string, elements: any[]): Promise<any> {
-        throw new Error('Method not implemented.')
-    }
-    transGo(elements: any, isAsync?: boolean): Promise<any> {
-        throw new Error('Method not implemented.')
-    }
-
-    private execQuery(tablename, params): Promise<any> {
         return new Promise((resolve, reject) => {
             MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
                 if (err) {
@@ -65,6 +46,45 @@ export default class MongoDao implements IDao {
                 }
             })
         })
+    }
+    update(tablename: string, params: object, id: string | number): Promise<any> {
+        return new Promise((resolve, reject) => {
+            MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+                if (err) {
+                    reject(G.jsResponse(G.STCODES.DATABASECOERR, err.message))
+                    G.logger.error(err.message)
+                } else {
+                    const db = client.db(dbName)
+                    db.collection(tablename).updateOne({_id: new ObjectId(id)}, {$set: params}, function (err, r) {
+                        if (err) {
+                            reject(G.jsResponse(G.STCODES.DATABASEOPERR, err.message))
+                            G.logger.error(err.message)
+                        } else {
+                            resolve(G.jsResponse(G.STCODES.SUCCESS, 'create success.', {
+                                affectedRows: r.result.n
+                            }))
+                            G.logger.debug(`update object ${JSON.stringify(params)} success.`)
+                            client.close()
+                        }
+                    })
+                }
+            })
+        })
+    }
+    delete(tablename: string, id: string | number): Promise<any> {
+        throw new Error('Method not implemented.')
+    }
+    querySql(sql: string, values: any[], params: object, fields?: string[]): Promise<any> {
+        throw new Error('Method not implemented.')
+    }
+    execSql(sql: string, values: any[]): Promise<any> {
+        throw new Error('Method not implemented.')
+    }
+    insertBatch(tablename: string, elements: any[]): Promise<any> {
+        throw new Error('Method not implemented.')
+    }
+    transGo(elements: any, isAsync?: boolean): Promise<any> {
+        throw new Error('Method not implemented.')
     }
 
 }
