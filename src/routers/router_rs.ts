c7644b2373f54f35c3fe4f5c61b7c344222410b2
zhoutk <zhoutk@189.cn>
2019-03-21 11:56:57 +0800

    rs route check v_ views.

--- src/routers/router_rs.ts
+++ src/routers/router_rs.ts
@@ -1,4 +1,5 @@
 import * as Router from 'koa-router'
+import BaseDao from '../db/baseDao'
 let router = new Router()
 
 const METHODS = {
@@ -25,13 +26,29 @@ export default (() => {
             }
         }
 
-        let module = loadModule(`../dao/${tableName}`)
+        let module = loadModule(`../dao/${tableName}`), is_module_exist = true
         if (!module) {
+            is_module_exist = false
             module = require('../db/baseDao')
         }
-        let db = new module.default(tableName)
 
-        ctx.body = await db[METHODS[method]](restParams, fields, ctx.session)
+        if (method === 'GET' && !tableName.startsWith('v_') && (!is_module_exist || 
+            is_module_exist && !Object.getOwnPropertyNames(module.default.prototype).some((al) => al === 'retrieve')) ) {
+            let rs = await new BaseDao().querySql(
+                'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_SCHEMA= ? and TABLE_NAME= ? ',
+                [G.CONFIGS.dbconfig.db_name, 'v_' + tableName])
+            if (rs.status === 200)
+                tableName = 'v_' + tableName
+        }
+
+        let rs: any
+        try {
+            let db = new module.default(tableName)
+            rs = await db[METHODS[method]](restParams, fields, ctx.session)
+        } catch (err) {
+            rs = G.jsResponse(G.STCODES.EXCEPTION, err.message, {stack: err.stack})
+        }
+        ctx.body = rs
         // ctx.body = await new BaseDao().execSql("insert into users (username, password, age) values (?,?,?) ", ['alice', 122, 16])          //test execSql create
         // ctx.body = await new BaseDao().execSql("update users set age = ? where id = ? ", [22, 1])          //test execSql update
         // ctx.body = await new BaseDao().querySql("select * from users where age = ? ", [12], params)       //test querySql
