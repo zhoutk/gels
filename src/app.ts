7c59de94e5f3a08db1ea32b847fc8c82db6105b3
zhoutk <zhoutk@189.cn>
2019-04-04 11:16:15 +0800

    no log for graphql request. remove apollo server koa.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import * as Koa from 'koa'
 import Startup from './inits'
-import GraphQl from './inits/graphql'
 
 //加载中间件
 export default {
@@ -8,7 +7,6 @@ export default {
     const app = new Koa()
     //支持 X-Forwarded-Host
     app.proxy = true
-    await new GraphQl().init(app)
     const middlewares = [
       'cors',
       'logger', //记录所用方式与时间
