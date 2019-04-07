50c56a9bf962709eddc8fe9f4f64acac2cc01872
zhoutk <zhoutk@189.cn>
2019-04-07 16:22:03 +0800

    sub table query ok.

--- src/middlewares/graphql/schema_generate.ts
+++ src/middlewares/graphql/schema_generate.ts
@@ -12,22 +12,22 @@ async function getInfoFromSql() {
     }
     let dao = new BaseDao()
     let tables = await dao.querySql('select TABLE_NAME,TABLE_COMMENT from information_schema.`TABLES` ' +
-    ' where TABLE_SCHEMA = ? and TABLE_TYPE = ? and substr(TABLE_NAME,1,2) <> ? order by ?', 
-    [G.CONFIGS.dbconfig.db_name, 'BASE TABLE', 't_', 'TABLE_NAME'])
+        ' where TABLE_SCHEMA = ? and TABLE_TYPE = ? and substr(TABLE_NAME,1,2) <> ? order by ?',
+        [G.CONFIGS.dbconfig.db_name, 'BASE TABLE', 't_', 'TABLE_NAME'])
     let columnRs = []
     tables.data.forEach((table) => {
         columnRs.push(dao.querySql('SELECT	`COLUMNS`.COLUMN_NAME,`COLUMNS`.COLUMN_TYPE,`COLUMNS`.IS_NULLABLE,' +
-        '`COLUMNS`.CHARACTER_SET_NAME,`COLUMNS`.COLUMN_DEFAULT,`COLUMNS`.EXTRA,' +
-        '`COLUMNS`.COLUMN_KEY,`COLUMNS`.COLUMN_COMMENT,`STATISTICS`.TABLE_NAME,' +
-        '`STATISTICS`.INDEX_NAME,`STATISTICS`.SEQ_IN_INDEX,`STATISTICS`.NON_UNIQUE,' +
-        '`COLUMNS`.COLLATION_NAME ' +
-        'FROM information_schema.`COLUMNS` ' +
-        'LEFT JOIN information_schema.`STATISTICS` ON ' +
-        'information_schema.`COLUMNS`.TABLE_NAME = `STATISTICS`.TABLE_NAME ' +
-        'AND information_schema.`COLUMNS`.COLUMN_NAME = information_schema.`STATISTICS`.COLUMN_NAME ' +
-        'AND information_schema.`STATISTICS`.table_schema = ? ' +
-        'where information_schema.`COLUMNS`.TABLE_NAME = ? and `COLUMNS`.table_schema = ?',
-        [G.CONFIGS.dbconfig.db_name, table.TABLE_NAME, G.CONFIGS.dbconfig.db_name]))
+            '`COLUMNS`.CHARACTER_SET_NAME,`COLUMNS`.COLUMN_DEFAULT,`COLUMNS`.EXTRA,' +
+            '`COLUMNS`.COLUMN_KEY,`COLUMNS`.COLUMN_COMMENT,`STATISTICS`.TABLE_NAME,' +
+            '`STATISTICS`.INDEX_NAME,`STATISTICS`.SEQ_IN_INDEX,`STATISTICS`.NON_UNIQUE,' +
+            '`COLUMNS`.COLLATION_NAME ' +
+            'FROM information_schema.`COLUMNS` ' +
+            'LEFT JOIN information_schema.`STATISTICS` ON ' +
+            'information_schema.`COLUMNS`.TABLE_NAME = `STATISTICS`.TABLE_NAME ' +
+            'AND information_schema.`COLUMNS`.COLUMN_NAME = information_schema.`STATISTICS`.COLUMN_NAME ' +
+            'AND information_schema.`STATISTICS`.table_schema = ? ' +
+            'where information_schema.`COLUMNS`.TABLE_NAME = ? and `COLUMNS`.table_schema = ?',
+            [G.CONFIGS.dbconfig.db_name, table.TABLE_NAME, G.CONFIGS.dbconfig.db_name]))
     })
     let rs = await Promise.all(columnRs), len = tables.data.length
     let querys = []
@@ -38,15 +38,15 @@ async function getInfoFromSql() {
         let paramStr = []
         columns.forEach((col) => {
             if (!col['COLUMN_NAME'].endsWith('_id')) {
-                colStr += `${col['COLUMN_NAME']}: ${TYPEFROMMYSQLTOGRAPHQL[G.tools.getStartTillBracket(col['COLUMN_TYPE'])]}
-                `
+                colStr += `${col['COLUMN_NAME']}: ${TYPEFROMMYSQLTOGRAPHQL[G.tools.getStartTillBracket(col['COLUMN_TYPE'])]}\n`
             } else {
-                colStr += `${G.L.trimEnd(col['COLUMN_NAME'], '_id')}: ${G.tools.bigCamelCase(G.L.trimEnd(col['COLUMN_NAME'], '_id'))}
-                `
-                resolvers[G.tools.bigCamelCase(table)][G.L.trimEnd(col['COLUMN_NAME'], '_id')] = async (al) => {
-                    let rs = await new BaseDao(G.L.trimEnd(col['COLUMN_NAME'], '_id')).retrieve({id: al[col['COLUMN_NAME']]})
-                    return rs.data[0]
-                  }
+                colStr += `${G.L.trimEnd(col['COLUMN_NAME'], '_id')}: ${G.tools.bigCamelCase(G.L.trimEnd(col['COLUMN_NAME'], '_id'))}\n`
+                resolvers[G.tools.bigCamelCase(table)] = {
+                    [G.L.trimEnd(col['COLUMN_NAME'], '_id')]: async (element) => {
+                        let rs = await new BaseDao(G.L.trimEnd(col['COLUMN_NAME'], '_id')).retrieve({ id: element[col['COLUMN_NAME']] })
+                        return rs.data[0]
+                    }
+                }
             }
             paramStr.push(`${col['COLUMN_NAME']}: ${TYPEFROMMYSQLTOGRAPHQL[G.tools.getStartTillBracket(col['COLUMN_TYPE'])]}`)
         })
@@ -55,12 +55,11 @@ async function getInfoFromSql() {
                 ${colStr}
             }
         `)
-        querys.push(`${table}s(${paramStr.join(', ')}): [${G.tools.bigCamelCase(table)}]
-        `)
+        querys.push(`${table}s(${paramStr.join(', ')}): [${G.tools.bigCamelCase(table)}]\n`)
         resolvers.Query[`${table}s`] = async (_, args) => {
             let rs = await new BaseDao(table).retrieve(args)
             return rs.data
-          }
+        }
     }
     typeDefs.push(`
         type Query {
